c8cd1c438d684c70f441db65b5f2922305278c93
Jisoo <ddsy999@gmail.com>
2023-05-20 17:09:40 +0900

    main1

--- operation.py
+++ operation.py
@@ -1,9 +1,62 @@
+import cv2
+import numpy as np
+from camClass import *
+from pynput import keyboard
+
+# 키 입력 이벤트 핸들러
+def on_key_press(key):
+    try:
+        if key == keyboard.Key.esc:  # ESC 키 체크
+            print('ESC 키를 눌러 프로그램을 종료합니다.')
+            listener.stop()  # 리스너 중지
+        elif key.char == 'a':
+            print('a')
+        elif key.char == 's':
+            print('s')
+        elif key.char == 'd':
+            print('d')
+        elif key.char == 'w':
+            print('w')
+    except AttributeError:
+        pass
+    
+    
+#0. Start 버튼 대기
+user_input = input("시작하려면 아무 키나 입력하세요: ")
+# 키 입력 이벤트 핸들러 등록
+listener = keyboard.Listener(on_press=on_key_press)
+listener.start()
+print("시작합니다")
 
 
-from camClass import camClass
+#1. cam 연결 시도
+print("Cam 상태 확인 중")
+try:
+    computerCam = camClass(ip=0, flip=False)
+except:
+    print("Cam 연결 실패")
+finally:
+    print("Cam 연결 성공: " + str(computerCam.captureValid))
 
-cam1 = camClass()
+user_input = input("Dual Cam을 시작하려면 q를 입력하세요: ")
 
-cam1.run()
 
-#cv2.destroyAllWindows()
+#2. Dual Cam Window
+if user_input == 'q':
+    print("Dual Cam 시작")
+    computerCam.show(maskedview=True)
+
+    print("Dual Cam 종료")
+
+
+#3. sv105 cam window 실행 & 자동추적
+user_input = input("자동 추적을 시작하려면 q를 입력하세요: ")
+if user_input == 'q':
+    h_min, h_max, s_min, s_max, v_min, v_max = 93, 142, 128, 255, 49, 255
+    computerCam.noCamDectect(h_min, h_max, s_min, s_max, v_min, v_max)
+
+    
+
+computerCam.release()
+# 키 입력 이벤트 리스너 중지
+listener.stop()
--- raspLegoHat.py
+++ raspLegoHat.py
@@ -1,67 +1,68 @@
-
-import cv2 
-import numpy as np 
-import keyboard
+import cv2
+import numpy as np
 from camClass import *
-import threading
-#capturePiCam = cv2.VideoCapture('http://localhost:8081')
-#captureSV105 = cv2.VideoCapture(1) #1은 SV105
-
-##################################################
-#0. Start 버튼 대기 
-#1. cam 연결 시도 
-#1-1. cam 연결 상태 print 
-#2. cam Window Big and small
-#2-1. 위치 조정 후 Dual 캠 종료
-#3. sv105 cam window 실행 & 자동추적 
-##################################################
-
-def on_key_press(event):
-    if event.name == 'a':
-        print('a')
-    if event.name == 's':
-        print('s')
-    if event.name == 'd':
-        print('d')
-    if event.name == 'w':
-        print('w')
+from pynput import keyboard
 
 
-#0. Start 버튼 대기 
-user_input = input("시작하려면 아무키나 입력하세요")
+from buildhat import Motor
+motorUD = Motor('C')
+motorLR = Motor('D')
+
+# 키 입력 이벤트 핸들러
+def on_key_press(key):
+    try:
+        if key == keyboard.Key.esc:  # ESC 키 체크
+            print('ESC 키를 눌러 프로그램을 종료합니다.')
+            listener.stop()  # 리스너 중지
+        elif key.char == 'a':
+            print('a')
+        elif key.char == 's':
+            motorLR.run_for_seconds(5,speed=10)
+            print('s')
+        elif key.char == 'd':
+            print('d')
+        elif key.char == 'w':
+            print('w')
+    except AttributeError:
+        pass
+    
+    
+#0. Start 버튼 대기
+user_input = input("시작하려면 아무 키나 입력하세요: ")
+# 키 입력 이벤트 핸들러 등록
+listener = keyboard.Listener(on_press=on_key_press)
+listener.start()
 print("시작합니다")
 
 
 #1. cam 연결 시도
-#1-1. cam 연결 상태 print 
-print("Cam 상태 확인중")
+print("Cam 상태 확인 중")
 try:
-    computerCam = camClass(ip='http://localhost:8081',flip=False)
+    computerCam = camClass(ip='http://localhost:8081', flip=False)
 except:
-    print("cam Failed")
+    print("Cam 연결 실패")
 finally:
-    print("cam Success " + str(computerCam.captureValid))
-    
-user_input = input("Dual Cam을 시작하려면 s 를 입력하세요 ")
+    print("Cam 연결 성공: " + str(computerCam.captureValid))
+
+user_input = input("Dual Cam을 시작하려면 q를 입력하세요: ")
+
 
 #2. Dual Cam Window
-if user_input=='y' or user_input=='Y':
+if user_input == 'q':
     print("Dual Cam 시작")
     computerCam.show(maskedview=True)
-    #2-1. 위치 조정
-    # 키 입력 이벤트 핸들러 등록
-    keyboard.on_press(on_key_press)
-    # 키 입력 이벤트 핸들러 제거
-    keyboard.unhook_all()
+
+    print("Dual Cam 종료")
+
+
+#3. sv105 cam window 실행 & 자동추적
+user_input = input("자동 추적을 시작하려면 q를 입력하세요: ")
+if user_input == 'q':
+    h_min, h_max, s_min, s_max, v_min, v_max = 93, 142, 128, 255, 49, 255
+    computerCam.noCamDectect(h_min, h_max, s_min, s_max, v_min, v_max)
+
     
-print("Dual Cam 종료")
-#3. sv105 cam window 실행 & 자동추적 
-user_input = input("자동 추적을 시작하려면 s 를 입력하세요 ")
-if user_input=='Y' or user_input=='y':
-    h_min,h_max,s_min,s_max,v_min,v_max = 93,142,128,255,49,255
 
-    computerCam.noCamDectect(h_min,h_max,s_min,s_max,v_min,v_max)
-
-    computerCam.release()
-
-
+computerCam.release()
+# 키 입력 이벤트 리스너 중지
+listener.stop()
