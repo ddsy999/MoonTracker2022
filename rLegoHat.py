0ea578edad8170451c73dd71a32c49ad985d9670
Jisoo <ddsy999@gmail.com>
2023-05-20 17:24:01 +0900

    main1

--- rLegoHat.py
+++ rLegoHat.py
@@ -12,7 +12,7 @@ motorLR = Motor('D')
 def on_key_press(key):
     try:
         if key == keyboard.Key.esc:  # ESC 키 체크
-            print('ESC 키를 눌러 프로그램을 종료합니다.')
+            print('ESC Key : Keyboad Listener End')
             listener.stop()  # 리스너 중지
         elif key.char == 'a':
             print('a')
@@ -28,41 +28,40 @@ def on_key_press(key):
     
     
 #0. Start 버튼 대기
-user_input = input("시작하려면 아무 키나 입력하세요: ")
+user_input = input("Stat press Any Key ")
 # 키 입력 이벤트 핸들러 등록
 listener = keyboard.Listener(on_press=on_key_press)
 listener.start()
-print("시작합니다")
+print("Start")
 
 
 #1. cam 연결 시도
-print("Cam 상태 확인 중")
+print("Confirming Cam Status ")
 try:
     computerCam = camClass(ip='http://localhost:8081', flip=False)
 except:
-    print("Cam 연결 실패")
+    print("Cam Connect Fail")
 finally:
-    print("Cam 연결 성공: " + str(computerCam.captureValid))
+    print("Cam Connet Succ: " + str(computerCam.captureValid))
 
-user_input = input("Dual Cam을 시작하려면 q를 입력하세요: ")
+user_input = input("Dual Cam Start Press  q : ")
 
 
 #2. Dual Cam Window
 if user_input == 'q':
-    print("Dual Cam 시작")
+    print("Dual Cam Start")
     computerCam.show(maskedview=True)
-
-    print("Dual Cam 종료")
+    print("Dual Cam End")
 
 
 #3. sv105 cam window 실행 & 자동추적
-user_input = input("자동 추적을 시작하려면 q를 입력하세요: ")
-if user_input == 'q':
-    h_min, h_max, s_min, s_max, v_min, v_max = 93, 142, 128, 255, 49, 255
-    computerCam.noCamDectect(h_min, h_max, s_min, s_max, v_min, v_max)
+#user_input = input("자동 추적을 시작하려면 q를 입력하세요: ")
+#if user_input == 'q':
+#    h_min, h_max, s_min, s_max, v_min, v_max = 93, 142, 128, 255, 49, 255
+#    computerCam.noCamDectect(h_min, h_max, s_min, s_max, v_min, v_max)
 
     
 
-computerCam.release()
+#computerCam.release()
 # 키 입력 이벤트 리스너 중지
 listener.stop()
