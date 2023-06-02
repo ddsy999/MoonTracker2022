d05a98e57dc213217924a088a5fcc2ef6672ec27
Jisoo <ddsy999@gmail.com>
2023-06-02 21:34:20 +0900

    main1

--- rLegoHat.py
+++ rLegoHat.py
@@ -37,43 +37,38 @@ listener.start()
 print("Start")
 
 
-#1. cam 연결 시도
-print("Confirming Cam Status ")
-try:
-    HSVListValue = [93,142,128,255,49,255]
-    sv105Cam = camClass(ip=1,flip=False,HSVList=HSVListValue,contourMasked=False,targetBox=True)
-    piCam = camClass(ip='http://localhost:8081',flip=False,HSVList=HSVListValue,contourMasked=False)
-    print("piCam Connet Succ: " + str(piCam.captureValid))
-    print("sv105Cam Connet Succ: " + str(sv105Cam.captureValid))
-except:
-    print("piCam Connect Fail")
+# #1. cam 연결 시도
+# print("Confirming Cam Status ")
+# try:
+#     HSVListValue = [93,142,128,255,49,255]
+#     sv105Cam = camClass(ip=1,flip=False,HSVList=HSVListValue,contourMasked=False,targetBox=True)
+#     piCam = camClass(ip='http://localhost:8081',flip=False,HSVList=HSVListValue,contourMasked=False)
+#     print("piCam Connet Succ: " + str(piCam.captureValid))
+#     print("sv105Cam Connet Succ: " + str(sv105Cam.captureValid))
+# except:
+#     print("piCam Connect Fail")
 
-user_input = input("Dual Cam Start Press  q : ")
-
-
-#2. Dual Cam Window
-if user_input == 'q':
-    print("Dual Cam Start")
-    camClass.totalShow()
-    print("Dual Cam End")
+# user_input = input("Dual Cam Start Press  q : ")
 
 
+# #2. Dual Cam Window
+# if user_input == 'q':
+#     print("Dual Cam Start")
+#     camClass.totalShow()
+#     print("Dual Cam End")
+# else :
+#     pass
 
 
 
-#0. Start 버튼 대기 
-user_input = input("Stat press Any Key ")
-print("Start")
-
-
-#1. cam 연결 시도
-#1-1. cam 연결 상태 print 
+#1. cam connect
+#1-1. cam connect print 
 
 print("Confirming Cam Status")
 try:
     HSVListValue = [93,142,128,255,49,255]
     sv105Cam = camClass(ip=1,flip=False,HSVList=HSVListValue,contourMasked=False,targetBox=True)
-    piCam = camClass(ip='http://localhost:8081',flip=False,HSVList=HSVListValue,contourMasked=False)
+    piCam = camClass(ip='http://localhost:8081',flip=False,HSVList=HSVListValue,contourMasked=False,fx=0.25,fy=0.25)
 except:
     print("cam Failed")
 finally:
